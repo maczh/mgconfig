d5ea63e81d0501030279cee88f78c65fb079b42e
maczh <Micro!070804>
2022-06-09 20:59:42 +0800

    Redis使用自带的连接池

--- redis.go
+++ redis.go
@@ -1,18 +1,16 @@
 package mgconfig
 
 import (
-	"errors"
 	"github.com/go-redis/redis"
 	"github.com/knadh/koanf"
 	"github.com/knadh/koanf/parsers/yaml"
 	"github.com/knadh/koanf/providers/rawbytes"
 	"github.com/levigross/grequests"
-	"github.com/silenceper/pool"
+	"net"
 	"time"
 )
 
 var Redis *redis.Client
-var redisPool pool.Pool
 
 func redisInit() {
 	if Redis == nil {
@@ -24,45 +22,39 @@ func redisInit() {
 			Addr:     cfg.String("go.data.redis.host") + ":" + cfg.String("go.data.redis.port"),
 			Password: cfg.String("go.data.redis.password"),
 			DB:       cfg.Int("go.data.redis.database"),
+			Dialer: func() (net.Conn, error) {
+				netDialer := &net.Dialer{
+					Timeout:   5 * time.Second,
+					KeepAlive: 5 * time.Minute,
+				}
+				return netDialer.Dial("tcp", cfg.String("go.data.redis.host")+":"+cfg.String("go.data.redis.port"))
+			},
 		}
-		Redis = redis.NewClient(&ro)
-		if err := Redis.Ping().Err(); err != nil {
-			logger.Error("Redis连接失败:" + err.Error())
-		}
-		if cfg.Int("go.data.redis_pool.max") > 1 && (redisPool == nil || redisPool.Len() == 0) {
-			factory := func() (interface{}, error) { return redisFactory(ro) }
-			close := func(v interface{}) error { return v.(*redis.Client).Close() }
-			ping := func(v interface{}) error { return v.(*redis.Client).Ping().Err() }
+		if cfg.Int("go.data.redis_pool.max") > 1 {
 			min := cfg.Int("go.data.redis_pool.min")
 			if min == 0 {
 				min = 2
 			}
+			ro.MinIdleConns = min
 			max := cfg.Int("go.data.redis_pool.max")
 			if max < min {
 				max = 10
 			}
-			idle := cfg.Int("go.data.redis_pool.idle")
-			if idle == 0 || idle > max {
-				idle = max / 2
-			}
-			idleTimeout := cfg.Int("go.data.redis_pool.timeout")
+			ro.PoolSize = max
+			idleTimeout := cfg.Int("go.data.redis_pool.idleTimeout")
 			if idleTimeout == 0 {
-				idleTimeout = 60
+				idleTimeout = 5
 			}
-			poolConfig := &pool.Config{
-				InitialCap:  min,
-				MaxCap:      max,
-				MaxIdle:     idle,
-				Factory:     factory,
-				Close:       close,
-				Ping:        ping,
-				IdleTimeout: time.Duration(idleTimeout) * time.Second,
+			ro.IdleTimeout = time.Duration(idleTimeout) * time.Minute
+			connectTimeout := cfg.Int("go.data.redis_pool.timeout")
+			if connectTimeout == 0 {
+				connectTimeout = 60
 			}
-			var err error
-			redisPool, err = pool.NewChannelPool(poolConfig)
-			if err != nil {
-				logger.Error("Redis连接池初始化错误")
-			}
+			ro.DialTimeout = time.Duration(connectTimeout) * time.Second
+		}
+		Redis = redis.NewClient(&ro)
+		if err := Redis.Ping().Err(); err != nil {
+			logger.Error("Redis连接失败:" + err.Error())
 		}
 	}
 }
@@ -70,9 +62,6 @@ func redisInit() {
 func redisClose() {
 	Redis.Close()
 	Redis = nil
-	if redisPool != nil && redisPool.Len() > 0 {
-		redisPool.Release()
-	}
 }
 
 func RedisCheck() {
@@ -88,37 +77,8 @@ func RedisCheck() {
 }
 
 func GetRedisConnection() *redis.Client {
-	if redisPool == nil {
-		logger.Error("未初始化Redis连接池")
-		redisInit()
-		return Redis
-	}
-	conn, err := redisPool.Get()
-	if err != nil {
-		logger.Error("获取Redis连接池中的连接失败:" + err.Error())
-		return nil
-	}
-	if conn == nil {
-		return nil
-	}
-	return conn.(*redis.Client)
+	return Redis
 }
 
 func ReturnRedisConnection(conn *redis.Client) {
-	if redisPool == nil || conn == nil {
-		redisInit()
-		return
-	}
-	err := redisPool.Put(conn)
-	if err != nil {
-		logger.Error("归还Redis连接给连接池错误:" + err.Error())
-	}
-}
-
-func redisFactory(ro redis.Options) (*redis.Client, error) {
-	goredis := redis.NewClient(&ro)
-	if goredis == nil {
-		return nil, errors.New("Redis Connection failed")
-	}
-	return goredis, goredis.Ping().Err()
 }
